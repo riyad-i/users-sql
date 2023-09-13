a4fd258725d7f711df94c3634afe27e991801035
Riyad <Riyad9876@gmail.com>
2023-09-13 10:47:37 -0400

    utils/db.js, sequelize instance

---
+++ utils/db.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+const {Sequelize} = require('sequelize')
+
+
+
+
+
+const sequelizeDB = new Sequelize('test', process.env.DB_USER, process.env.DB_PASSWORD, {
+    dialect: 'mysql',
+    host: process.env.DATABASE_URL,
+    port: 3306,
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+})
+
+
+module.exports= sequelizeDB
